90bc75a268b62a0c8a9768a163334d296a3e0b9e
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-15 17:32:45 +0100

    fixing state lock config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "resume-website-terraform-state-15-08-2024"
     key            = "terraform.tfstate"
     region         = "eu-north-1"
-    dynamodb_table = "kwabena-resume-website-terraform-lock"
+    
     }
 }
