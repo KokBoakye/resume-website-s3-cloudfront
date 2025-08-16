16cc638f438ed8a8aca02caf646c6c83d4ff90b9
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-16 17:02:26 +0100

    ACM certificate

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-    bucket         = "resume-website-terraform-state-15-08-2024"
+    bucket         = "resume-website-terraform-state-16-08-2024"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     
