cd9b66f42c8705c7635d2ed36c79a5f17c0ecb70
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-16 11:56:53 +0100

    pushing web files

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,3 +41,69 @@ resource "aws_s3_bucket" "resume_website" {
 #   ignore_public_acls      = false
 #   restrict_public_buckets = false
 # }
+
+resource "aws_cloudfront_origin_access_identity" "resume_website_oai" {
+  comment = "OAI for Resume Website"
+}
+
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.resume_website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.resume_website_oai.iam_arn
+        }
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.resume_website.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudfront_distribution" "resume_website_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.resume_website.bucket_regional_domain_name
+    origin_id   = "resume_website_origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.resume_website_oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "resume_website_origin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "s3_endpoint" {
   description = "The S3 endpoint for the resume website"
   value       = aws_s3_bucket.resume_website.website_endpoint
 }
+
+output "cloudfront_url" {
+  description = "The CloudFront URL for the resume website"
+  value       = aws_cloudfront_distribution.resume_website_distribution.domain_name
+}
