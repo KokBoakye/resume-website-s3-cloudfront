3e17a92a08022c3cffc76f23182c50504711efc8
Johnson <105896538+Efotsowoe@users.noreply.github.com>
2025-08-17 07:59:42 -0600

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,3 +6,6 @@ terraform {
     
     }
 }
+
+# I was hoping to see the terraform state locking argument. Since the use of DynamoDB is deprecated, you would have used the `use_lockfile` argument to achieve that result.
+# More so, please desist from putting the terraform.tfstate file in the root of the s3 bucket. It should be placed in a folder. 
