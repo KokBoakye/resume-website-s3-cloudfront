9a1b2cb2ce0025cb81d7dbd20eb321e598cf802b
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-18 22:29:21 +0100

    push work files

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "resume-website-terraform-state-16-08-2024"
     key            = "/resume-website-backend/terraform.tfstate"
     region         = "us-east-1"
-    use_lockfile = true
+
     
     }
 }
