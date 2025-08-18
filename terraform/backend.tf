6e3dda49c9dea3fbbff4da1fb5a5b6ba7bde0ca2
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-18 22:26:21 +0100

    push work files

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,10 @@
 terraform {
     backend "s3" {
     bucket         = "resume-website-terraform-state-16-08-2024"
-    key            = "terraform.tfstate"
+    key            = "/resume-website-backend/terraform.tfstate"
     region         = "us-east-1"
+    use_lockfile = true
     
     }
 }
 
-# I was hoping to see the terraform state locking argument. Since the use of DynamoDB is deprecated, you would have used the `use_lockfile` argument to achieve that result.
-# More so, please desist from putting the terraform.tfstate file in the root of the s3 bucket. It should be placed in a folder. 
