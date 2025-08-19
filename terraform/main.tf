2f9b58c6ec1feda3d42cc030351e5a908beb11e0
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-19 11:08:14 +0100

    acm certificate validation

--- terraform/main.tf
+++ terraform/main.tf
@@ -171,3 +171,25 @@ resource "aws_acm_certificate" "resume_website_cert" {
     create_before_destroy = true
   }
 }
+
+resource "aws_route53_record" "resume_website_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.resume_website_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+      zone_id = aws_route53_zone.resume_website.zone_id
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
+  zone_id = each.value.zone_id
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "resume_website_cert_validation" {
+  certificate_arn         = aws_acm_certificate.resume_website_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.resume_website_cert_validation : record.fqdn]
+}
