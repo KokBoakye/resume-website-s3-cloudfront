7180ce0cf84c89de1a564a4d7c92ac11fb976a52
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-31 21:07:18 +0100

    refactor: commented out manifest in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,15 @@
-output "s3_endpoint" {
-  description = "The S3 endpoint for the resume website"
-  value       = aws_s3_bucket.resume_website.website_endpoint
-}
-
-output "cloudfront_url" {
-  description = "The CloudFront URL  for the resume website"
-  value       = aws_cloudfront_distribution.resume_website_distribution.domain_name
-}
-
-# output "alias_domain_name" {
-#   description = "The alias domain name for the resume website"
-#   value       = aws_route53_record.resume_website_alias.fqdn
-  
+# output "s3_endpoint" {
+#   description = "The S3 endpoint for the resume website"
+#   value       = aws_s3_bucket.resume_website.website_endpoint
 # }
+
+# output "cloudfront_url" {
+#   description = "The CloudFront URL  for the resume website"
+#   value       = aws_cloudfront_distribution.resume_website_distribution.domain_name
+# }
+
+# # output "alias_domain_name" {
+# #   description = "The alias domain name for the resume website"
+# #   value       = aws_route53_record.resume_website_alias.fqdn
+
+# # }
